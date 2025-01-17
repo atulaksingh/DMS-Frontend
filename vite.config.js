348ace45aebb0db26fabd8d0bb254ad1d68987c5
atulaksingh <talktoatulsingh@gmail.com>
2025-01-17 18:12:41 +0530

    update package.json-2

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'/'
-  
+  base: '/',
+  build: {
+    rollupOptions: {
+      external: ['react-icons/im'], // Prevents Rollup from bundling this module
+    },
+  },
 })
-
-
-
