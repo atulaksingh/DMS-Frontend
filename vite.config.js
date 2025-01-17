4e27515be5e3e71d24f7b67c628080acb34feae1
atulaksingh <talktoatulsingh@gmail.com>
2025-01-17 18:28:54 +0530

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'), // Optional: Custom alias for cleaner imports
+    },
+  },
   build: {
     rollupOptions: {
-      external: ['react-icons/im'], // Prevents Rollup from bundling this module
+      external: [],
     },
   },
 })
