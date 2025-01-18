11778d0c16847b5bd7cf69ab4447b321f0f8c89f
atulaksingh <talktoatulsingh@gmail.com>
2025-01-18 10:17:12 +0530

    update homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { toast } from "react-toastify";
 import MuiTable from "../Components/MainCards/MuiTable";
 
 function HomePage() {
@@ -8,35 +7,28 @@ function HomePage() {
   const [loading, setLoading] = useState(true);
 
   const fetchClients = async () => {
-      try {
-          const response = await axios.get("http://127.0.0.1:8000/api/list-client");
-          // console.log("response",response.data)
-          setClients(response.data.clients); // Assuming the data is returned in the response body
-          setLoading(false);
-      } catch (error) {
-          console.error("Error fetching clients:", error);
-          toast.error("Failed to fetch client data.", {
-              position: "top-right",
-              autoClose: 2000,
-          });
-          setLoading(false);
-      }
+    try {
+      const response = await axios.get("http://127.0.0.1:8000/api/list-client");
+      setClients(response.data.clients); // Assuming the data is returned in the response body
+      setLoading(false);
+    } catch (error) {
+      console.error("Error fetching clients:", error);
+      // Keep loading state as true in case of a network error
+    }
   };
-  useEffect(() => {
 
-      fetchClients();
-  }, []); // Empty dependency array ensures this runs once when the component mounts
+  useEffect(() => {
+    fetchClients();
+  }, []);
 
   if (loading) {
-      return <div>Loading...</div>;
+    return <div>Loading...</div>;
   }
+
   return (
-    <>
-      <div >
-        
-        <MuiTable tableData={clients} fetchClients={fetchClients}/>
-      </div>
-    </>
+    <div>
+      <MuiTable tableData={clients} fetchClients={fetchClients} />
+    </div>
   );
 }
 
